348db4f98f0ef50bc42e327d926d3306207b6230
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-02-28 09:31:38 +0000

    Update ServerPop.cs
    ** REQUIRES NEW CONFIG RESET **
    
    Added option to show pop on connection
    
    Added option for welcome message 
    
    Added language file 
    
    Now supports Upper & Lower case !pop trigger 
    
    Fixed chat icon not being applied to player response

--- ServerPop.cs
+++ ServerPop.cs
@@ -1,68 +1,122 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("ServerPop", "Mabel", "1.0.4"), Description("Show server pop in chat with !pop trigger.")]
-    class ServerPop : RustPlugin
+    [Info("Server Pop", "Mabel", "1.0.5")]
+    [Description("Show server pop in chat with !pop trigger.")]
+    public class ServerPop : RustPlugin
     {
-        private bool showOnlinePlayers;
-        private bool showSleepingPlayers;
-        private bool showJoiningPlayers;
-        private bool showQueuedPlayers;
+        static Configuration config;
 
-        private string chatPrefix = "<size=16><color=#FFA500>| ServerPop |</color></size>";
-        private string valueColorHex = "#FFA500";
-
-        private ulong customSteamID = 0;
-        private bool globalResponse = true;
-
-        protected override void LoadDefaultConfig()
+        public class Configuration
         {
-            Config.Clear();
-            Config["ShowOnlinePlayers"] = true;
-            Config["ShowSleepingPlayers"] = true;
-            Config["ShowJoiningPlayers"] = true;
-            Config["ShowQueuedPlayers"] = true;
-            Config["ChatPrefix"] = chatPrefix;
-            Config["ValueColorHex"] = valueColorHex;
-            Config["ChatIconSteamID"] = customSteamID;
-            Config["GlobalResponse"] = globalResponse; // true = global response, false = player response
-            SaveConfig();
-        }
+            [JsonProperty(PropertyName = "Chat Prefix")]
+            public string chatPrefix { get; set; }
 
-        void Init()
-        {
-            LoadConfigValues();
-        }
+            [JsonProperty(PropertyName = "Value Color (HEX)")]
+            public string valueColor { get; set; }
 
-        void LoadConfigValues()
-        {
-            showOnlinePlayers = Config.Get<bool>("ShowOnlinePlayers");
-            showSleepingPlayers = Config.Get<bool>("ShowSleepingPlayers");
-            showJoiningPlayers = Config.Get<bool>("ShowJoiningPlayers");
-            showQueuedPlayers = Config.Get<bool>("ShowQueuedPlayers");
+            [JsonProperty(PropertyName = "Chat Icon SteamID")]
+            public ulong chatSteamID { get; set; } = 76561199216745239;
 
-            chatPrefix = Config.Get<string>("ChatPrefix");
-            valueColorHex = Config.Get<string>("ValueColorHex");
-            customSteamID = Config.Get<ulong>("ChatIconSteamID");
-            
-			object globalResponseObj = Config.Get("GlobalResponse");
-			if (globalResponseObj != null && globalResponseObj is bool)
-			{
-                globalResponse = (bool)globalResponseObj;
+            [JsonProperty(PropertyName = "GlobalResponse (true = global response, false = player response)")]
+            public bool globalResponse { get; set; }
+
+            [JsonProperty(PropertyName = "Show Online Players")]
+            public bool showOnlinePlayers { get; set; }
+
+            [JsonProperty(PropertyName = "Show Sleeping Players")]
+            public bool showSleepingPlayers { get; set; }
+
+            [JsonProperty(PropertyName = "Show Joining Players")]
+            public bool showJoiningPlayers { get; set; }
+
+            [JsonProperty(PropertyName = "Show Queued Players")]
+            public bool showQueuedPlayers { get; set; }
+
+            [JsonProperty(PropertyName = "Show Pop On Connect")]
+            public bool showPopOnConnect { get; set; }
+
+            [JsonProperty(PropertyName = "Show Welcome Message")]
+            public bool showWelcomeMessage { get; set; }
+
+            public static Configuration DefaultConfig()
+            {
+                return new Configuration
+                {
+                    chatPrefix = "<size=16><color=#FFA500>| Server Pop |</color></size>",
+                    valueColor = "#FFA500",
+                    chatSteamID = 76561199216745239,
+                    globalResponse = true,
+                    showOnlinePlayers = true,
+                    showSleepingPlayers = true,
+                    showJoiningPlayers = true,
+                    showQueuedPlayers = true,
+                    showPopOnConnect = false,
+                    showWelcomeMessage = false
+                };
             }
-            else
-            { 
-                globalResponse = true;
+        }
 
-                Config["GlobalResponse"] = globalResponse;
-                Config.Save();
-           }				
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                config = Config.ReadObject<Configuration>();
+                if (config == null) LoadDefaultConfig();
+                SaveConfig();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                PrintWarning("Creating new configuration file.....");
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void LoadDefaultConfig() => config = Configuration.DefaultConfig();
+        protected override void SaveConfig() => Config.WriteObject(config);
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Online"] = "{0} / {1} players online",
+                ["Sleeping"] = "{0} players sleeping",
+                ["Joining"] = "{0} players joining",
+                ["Queued"] = "{0} players queued",
+                ["WelcomeMessage"] = "Welcome to the server {0}!"
+            }, this);
+        }
+
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            if (config.showWelcomeMessage)
+            {
+                string welcomeMessage = lang.GetMessage("WelcomeMessage", this);
+
+                if (!string.IsNullOrEmpty(welcomeMessage))
+                {
+                    welcomeMessage = string.Format(welcomeMessage, player.displayName);
+
+                    Player.Message(player, welcomeMessage, config.chatSteamID);
+                }
+            }
+
+            if (config.showPopOnConnect)
+            {
+                SendMessage(player);
+            }
         }
 
         private void OnPlayerChat(BasePlayer player, string message, ConVar.Chat.ChatChannel channel)
         {
-            if (message == "!pop")
+            if (message.ToLower() == "!pop")
             {
                 SendMessage(player);
             }
@@ -70,31 +124,51 @@ namespace Oxide.Plugins
 
         private void SendMessage(BasePlayer player)
         {
-            StringBuilder popMessage = new StringBuilder($"{chatPrefix}\n\n");
+            StringBuilder popMessage = new StringBuilder($"{config.chatPrefix}\n\n");
 
-            if (showOnlinePlayers)
-                popMessage.AppendLine($"{ColorizeText($"{BasePlayer.activePlayerList.Count} / {ConVar.Server.maxplayers}", valueColorHex)} player's online\n");
-
-            if (showSleepingPlayers)
-                popMessage.AppendLine($"{ColorizeText(BasePlayer.sleepingPlayerList.Count.ToString(), valueColorHex)} player's sleeping\n");
-
-            if (showJoiningPlayers)
-                popMessage.AppendLine($"{ColorizeText(ServerMgr.Instance.connectionQueue.Joining.ToString(), valueColorHex)} player's joining\n");
-
-            if (showQueuedPlayers)
-                popMessage.AppendLine($"{ColorizeText(ServerMgr.Instance.connectionQueue.Queued.ToString(), valueColorHex)} player's queued\n");
-
-            if (globalResponse)
+            if (config.showOnlinePlayers)
             {
-                Server.Broadcast(popMessage.ToString(), null, customSteamID);
+                string onlinePlayersText = $"{BasePlayer.activePlayerList.Count} / {ConVar.Server.maxplayers}";
+                string onlineMessage = $"{lang.GetMessage("Online", this)}";
+                onlineMessage = string.Format(onlineMessage, ApplyColor(BasePlayer.activePlayerList.Count.ToString(), config.valueColor), ApplyColor(ConVar.Server.maxplayers.ToString(), config.valueColor));
+                popMessage.AppendLine($"{onlineMessage}\n");
+            }
+
+            if (config.showSleepingPlayers)
+            {
+                string sleepingPlayersText = BasePlayer.sleepingPlayerList.Count.ToString();
+                string sleepingMessage = $"{lang.GetMessage("Sleeping", this)}";
+                sleepingMessage = string.Format(sleepingMessage, ApplyColor(BasePlayer.sleepingPlayerList.Count.ToString(), config.valueColor));
+                popMessage.AppendLine($"{sleepingMessage}\n");
+            }
+
+            if (config.showJoiningPlayers)
+            {
+                string joiningPlayersText = ServerMgr.Instance.connectionQueue.Joining.ToString();
+                string joiningMessage = $"{lang.GetMessage("Joining", this)}";
+                joiningMessage = string.Format(joiningMessage, ApplyColor(ServerMgr.Instance.connectionQueue.Joining.ToString(), config.valueColor));
+                popMessage.AppendLine($"{joiningMessage}\n");
+            }
+
+            if (config.showQueuedPlayers)
+            {
+                string queuedPlayersText = ServerMgr.Instance.connectionQueue.Queued.ToString();
+                string queuedMessage = $"{lang.GetMessage("Queued", this)}";
+                queuedMessage = string.Format(queuedMessage, ApplyColor(ServerMgr.Instance.connectionQueue.Queued.ToString(), config.valueColor));
+                popMessage.AppendLine($"{queuedMessage}\n");
+            }
+
+            if (config.globalResponse)
+            {
+                Server.Broadcast(popMessage.ToString(), null, config.chatSteamID);
             }
             else
             {
-                player.ChatMessage(popMessage.ToString());
+                Player.Message(player, popMessage.ToString(), null, config.chatSteamID);
             }
         }
 
-        string ColorizeText(string text, string hexColor) 
+        private string ApplyColor(string text, string hexColor)
         {
             return $"<color={hexColor}>{text}</color>";
         }
