6209dd935a2f545ddaa02dbcba42585e60df5d59
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-04-22 09:43:45 +0100

    Update BlackjackFix.cs
    Added Owner Check

--- BlackjackFix.cs
+++ BlackjackFix.cs
@@ -1,11 +1,14 @@
 namespace Oxide.Plugins
 {
-    [Info("Blackjack Fix", "Mabel", "1.0.0")]
+    [Info("Blackjack Fix", "Mabel", "1.0.1")]
     [Description("Fixes Blackjack spawned in wrong rotation")]
     public class BlackjackFix : RustPlugin
     {
         private void OnEntitySpawned(BlackjackMachine entity)
         {
+            if (entity.OwnerID == 0)
+                return;
+
             var transform = entity.transform;
             transform.Rotate(0.0f, 180.0f, 0.0f);
         }
