d6e271580eb0c1b51c741f817b1c7f5b0cc5aadf
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-05-14 15:13:53 +0100

    Update BlackjackFix.cs
    Added Support For The Ferry Plugin

--- BlackjackFix.cs
+++ BlackjackFix.cs
@@ -1,13 +1,15 @@
 namespace Oxide.Plugins
 {
-    [Info("Blackjack Fix", "Mabel", "1.0.1")]
+    [Info("Blackjack Fix", "Mabel", "1.0.2")]
     [Description("Fixes Blackjack spawned in wrong rotation")]
     public class BlackjackFix : RustPlugin
     {
         private void OnEntitySpawned(BlackjackMachine entity)
         {
-            if (entity.OwnerID == 0)
+            if (entity.OwnerID == 0 || entity.OwnerID == 86753095)
+            {
                 return;
+            }
 
             var transform = entity.transform;
             transform.Rotate(0.0f, 180.0f, 0.0f);
