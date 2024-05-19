0a0390cf0bf8ac68c682dfbf71e006b9f087c8f0
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-05-19 22:33:59 +0100

    Update BlackjackFix.cs

--- BlackjackFix.cs
+++ BlackjackFix.cs
@@ -2,6 +2,7 @@ namespace Oxide.Plugins
 {
     [Info("Blackjack Fix", "Mabel", "1.0.2")]
     [Description("Fixes Blackjack spawned in wrong rotation")]
+    
     public class BlackjackFix : RustPlugin
     {
         private void OnEntitySpawned(BlackjackMachine entity)
@@ -10,7 +11,6 @@ namespace Oxide.Plugins
             {
                 return;
             }
-
             var transform = entity.transform;
             transform.Rotate(0.0f, 180.0f, 0.0f);
         }
