6322989ea629498dc210d75192ef630d7ac432a5
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-05-19 15:29:43 +0100

    Update NPCGifts.cs
    Complete rewrite & Added Support for Simple Loot Table 
    
    https://codefling.com/plugins/simple-loot-table

--- NPCGifts.cs
+++ NPCGifts.cs
@@ -2,77 +2,234 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Newtonsoft.Json;
+using Oxide.Core;
+using Oxide.Core.Plugins;
 
 namespace Oxide.Plugins
 {
-    [Info("NPCGifts", "Mabel", "1.0.2")]
+    [Info("NPC Gifts", "Mabel", "1.0.4")]
+    [Description("Set a chance to spawn a gift for players for killing NPC")]
+	
     class NPCGifts : RustPlugin
     {
-        private class ContainerData
-        {
-            public string Prefab { get; set; }
-            public float SpawnChance { get; set; }
-            public bool IsEnabled { get; set; }
-            public string Permission { get; set; }
-        }
+        [PluginReference] private readonly Plugin SimpleLootTable;
 
-        private class CooldownData
-        {
-            public ulong PlayerID { get; set; }
-            public DateTime LastSpawnTime { get; set; }
-        }
-
-        private List<ContainerData> containerList;
-        private Dictionary<ulong, CooldownData> cooldowns = new Dictionary<ulong, CooldownData>();
+        private List<Container> containerList;
         private List<BaseEntity> spawnedContainers = new List<BaseEntity>();
-        private float cooldownDurationMinutes = 60;
+        private float cooldownDurationMinutes;
         private System.Random random;
+        
+        static Configuration config;
+        public class Configuration
+        {
+            [JsonProperty(PropertyName = "Cooldown Settings")]
+            public CooldownSettings CooldownSettings { get; set; }
 
-        private string containerReceivedMessage;
+            [JsonProperty(PropertyName = "Message Settings")]
+            public MessageSettings MessageSettings { get; set; }
+
+            [JsonProperty(PropertyName = "Container Settings")]
+            public List<Container> Container { get; set; }
+
+            public Core.VersionNumber Version { get; set; }
+
+        }
+
+        public class CooldownSettings
+        {
+            [JsonProperty(PropertyName = "Cooldown Duration Minutes")]
+            public float cooldownDurationMinutes { get; set; }
+        }
+
+        public class MessageSettings
+        {
+            [JsonProperty(PropertyName = "Enable Chat Message")]
+            public bool chat;
+
+            [JsonProperty(PropertyName = "Enable Game Tip Message")]
+            public bool toast;
+        }
+
+        public class Container
+        {
+            [JsonProperty("Enabled")]
+            public bool Enabled { get; set; }
+
+            [JsonProperty("Container Prefab")]
+            public string ContainerType { get; set; }
+
+            [JsonProperty("Spawn Chance")]
+            public float SpawnChance { get; set; }
+
+            [JsonProperty("Permission")]
+            public string Permission { get; set; }
+
+            [JsonProperty("Loot Table Name")]
+            public string tableName { get; set; }
+
+            [JsonProperty("Min Items")]
+            public int minAmount { get; set; }
+
+            [JsonProperty("Max Items")]
+            public int maxAmount { get; set; }
+        }
+
+        public class VersionNumber
+        {
+            Core.VersionNumber Version { get; set; }
+        }
+
+        public static Configuration DefaultConfig()
+        {
+            return new Configuration()
+            {
+                CooldownSettings = new CooldownSettings
+                {
+                    cooldownDurationMinutes = 60f,
+                },
+
+                MessageSettings = new MessageSettings
+                {
+                    chat = false,
+                    toast = true
+                },
+
+                Container = new List<Container>
+                {
+                    new Container
+                    {
+                        Enabled = true,
+                        ContainerType = "assets/prefabs/misc/xmas/sleigh/presentdrop.prefab",
+                        SpawnChance = 0.5f,
+                        Permission = "npcgifts.example1",
+                        tableName = "",
+                        minAmount = 0,
+                        maxAmount = 0,
+                    },
+                    new Container
+                    {
+                        Enabled = true,
+                        ContainerType = "assets/prefabs/missions/portal/proceduraldungeon/xmastunnels/loot/xmastunnellootbox.prefab",
+                        SpawnChance = 0.5f,
+                        Permission = "npcgifts.example2",
+                        tableName = "",
+                        minAmount = 0,
+                        maxAmount = 0,
+                    },
+                    new Container
+                    {
+                        Enabled = true,
+                        ContainerType = "assets/prefabs/misc/xmas/giftbox/giftbox_loot.prefab",
+                        SpawnChance = 0.5f,
+                        Permission = "npcgifts.example3",
+                        tableName = "",
+                        minAmount = 0,
+                        maxAmount = 0,
+                    }
+                },
+                Version = new Core.VersionNumber()
+            };
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                config = Config.ReadObject<Configuration>();
+                if (config == null) LoadDefaultConfig();
+                SaveConfig();
+
+                if (config.Version < Version)
+                    UpdateConfig();
+
+                Config.WriteObject(config, true);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                PrintWarning("Creating new configuration file....");
+                LoadDefaultConfig();
+            }
+        }
 
         protected override void LoadDefaultConfig()
         {
-            Config["Containers"] = new List<object>
+            config = DefaultConfig();
+            PrintWarning("Default configuration has been loaded....");
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(config);
+        }
+
+        private void UpdateConfig()
+        {
+            PrintWarning("Config update detected! Updating config values...");
+
+            Configuration baseConfig = DefaultConfig();
+
+            if (config.Version < new Core.VersionNumber(1, 0, 1))
+                config = baseConfig;
+
+            config.Version = Version;
+
+            PrintWarning("Config update completed!");
+        }
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
             {
-                new Dictionary<string, object>
-                {
-                    ["Prefab"] = "assets/prefabs/misc/xmas/sleigh/presentdrop.prefab",
-                    ["SpawnChance"] = 0.5f,
-                    ["IsEnabled"] = true,
-                    ["Permission"] = "npcgifts.example1"
-                },
-                new Dictionary<string, object>
-                {
-                    ["Prefab"] = "assets/prefabs/missions/portal/proceduraldungeon/xmastunnels/loot/xmastunnellootbox.prefab",
-                    ["SpawnChance"] = 0.5f,
-                    ["IsEnabled"] = true,
-                    ["Permission"] = "npcgifts.example2"
-                },
-                new Dictionary<string, object>
-                {
-                    ["Prefab"] = "assets/prefabs/misc/xmas/giftbox/giftbox_loot.prefab",
-                    ["SpawnChance"] = 0.5f,
-                    ["IsEnabled"] = true,
-                    ["Permission"] = "npcgifts.example3"
-                }
-            };
-            Config["CooldownDurationMinutes"] = cooldownDurationMinutes;
-
-            containerReceivedMessage = Config.Get("ContainerReceivedMessage", " :santahat: Merry Christmas <color=#4A95CC>{player.name}</color> from <color=#4A95CC>{server.name}</color> :santahat:") as string;
-            Config["ContainerReceivedMessage"] = containerReceivedMessage;
-
-            SaveConfig();
+                ["ChatMessage"] = " :dance: Congratulations <color=#abf229>{0}</color> You Received A Gift From <color=#abf229>{1}</color> :dance:",
+                ["GameTipMessage"] = "Congratulations <color=#abf229>{0}</color> You Received A Gift From <color=#abf229>{1}</color>"
+            }, this);
         }
 
         void Init()
         {
-            LoadConfigValues();
-            cooldownDurationMinutes = Convert.ToSingle(Config["CooldownDurationMinutes"]);
             random = new System.Random();
 
+            if (config?.CooldownSettings != null)
+            {
+                cooldownDurationMinutes = config.CooldownSettings.cooldownDurationMinutes;
+            }
+
+            containerList = config?.Container ?? new List<Container>();
+
+            LoadCooldownData();
             CheckAndCreatePermissions();
 
             timer.Once(300f, () => CleanupContainers());
+        }
+
+        void Unload()
+        {
+            SaveCooldownData();
+            CleanupContainers();
+        }
+
+        private void LoadCooldownData()
+        {
+            var data = Interface.Oxide.DataFileSystem.ReadObject<Dictionary<string, object>>("NPCGiftsData");
+            if (data.TryGetValue("LastSpawnTimes", out var spawnTimes))
+            {
+                lastSpawnTimes = spawnTimes as Dictionary<ulong, DateTime> ?? new Dictionary<ulong, DateTime>();
+            }
+            else
+            {
+                lastSpawnTimes = new Dictionary<ulong, DateTime>();
+            }
+        }
+        private void SaveCooldownData()
+        {
+            var data = new Dictionary<string, object>
+            {
+                ["LastSpawnTimes"] = lastSpawnTimes
+            };
+            Interface.Oxide.DataFileSystem.WriteObject("NPCGiftsData", data);
         }
 
         private void CheckAndCreatePermissions()
@@ -89,61 +246,11 @@ namespace Oxide.Plugins
             }
         }
 
-        void LoadConfigValues()
-        {
-            object configObject = Config["Containers"];
-            if (configObject is List<object>)
-            {
-                containerList = (configObject as List<object>).Select(x =>
-                {
-                    var containerData = new ContainerData
-                    {
-                        Prefab = Convert.ToString(((Dictionary<string, object>)x).GetValueOrDefault("Prefab", string.Empty)),
-                        SpawnChance = Convert.ToSingle(((Dictionary<string, object>)x).GetValueOrDefault("SpawnChance", 0.0f)),
-                        IsEnabled = Convert.ToBoolean(((Dictionary<string, object>)x).GetValueOrDefault("IsEnabled", false)),
-                        Permission = Convert.ToString(((Dictionary<string, object>)x).GetValueOrDefault("Permission", string.Empty))
-                    };
-
-                    return containerData;
-                }).ToList();
-
-                if (containerList.Any(c => string.IsNullOrEmpty(c.Permission)))
-                {
-                    foreach (var containerData in containerList)
-                    {
-                        if (string.IsNullOrEmpty(containerData.Permission))
-                        {
-                            containerData.Permission = $"npcgifts.example{containerList.IndexOf(containerData) + 1}";
-                        }
-                    }
-
-                    Config["Containers"] = containerList.Cast<object>().ToList();
-                    SaveConfig();
-                    Puts("Added missing permissions to config.");
-                }
-            }
-            else
-            {
-                PrintWarning("Failed to load the configuration file. Using default values.");
-                LoadDefaultConfig();
-                SaveConfig();
-                LoadConfigValues();
-            }
-
-            containerReceivedMessage = Config.Get("ContainerReceivedMessage") as string;
-            if (containerReceivedMessage == null)
-            {
-                containerReceivedMessage = " :santahat: Merry Christmas <color=#4A95CC>{player.name}</color> from <color=#4A95CC>{server.name}</color> :santahat:";
-                Config["ContainerReceivedMessage"] = containerReceivedMessage;
-                SaveConfig();
-            }	
-        }
-
         private void OnEntityDeath(BaseCombatEntity entity, HitInfo hitInfo)
         {
             if (entity == null || hitInfo == null) return;
 
-            BaseEntity killer = hitInfo.Initiator as BaseEntity;
+            BaseEntity killer = hitInfo.Initiator;
             BasePlayer player = killer as BasePlayer;
 
             if (player != null && entity.GetComponent<HumanNPC>() != null)
@@ -162,26 +269,36 @@ namespace Oxide.Plugins
 
             random = new System.Random();
 
-            foreach (var containerData in containerList.Where(c => c.IsEnabled))
+            foreach (var containerData in containerList.Where(c => c.Enabled))
             {
                 if (random.NextDouble() <= containerData.SpawnChance)
                 {
                     if (string.IsNullOrEmpty(containerData.Permission) || permission.UserHasPermission(playerID.ToString(), containerData.Permission))
                     {
-                        BaseEntity container = GameManager.server.CreateEntity(containerData.Prefab, position);
+                        BaseEntity container = GameManager.server.CreateEntity(containerData.ContainerType, position);
 
                         if (container != null)
                         {
                             container.Spawn();
                             spawnedContainers.Add(container);
+                            GetSetItems(container, containerData.tableName, containerData.minAmount, containerData.maxAmount, 1f);
+
                             BasePlayer player = BasePlayer.FindByID(playerID);
                             if (player != null)
                             {
-                                SendChatMessage(player, ReplacePlaceholders(containerReceivedMessage, player));
+                                if (config.MessageSettings.chat)
+                                {
+                                    var containerReceivedMessage = lang.GetMessage("ChatMessage", this);
+                                    SendChatMessage(player, ReplacePlaceholders(player, containerReceivedMessage));
+                                }
+                                else if (config.MessageSettings.toast)
+                                {
+                                    var containerReceivedMessage = lang.GetMessage("GameTipMessage", this);
+                                    player.ShowToast(GameTip.Styles.Blue_Long, ReplacePlaceholders(player, containerReceivedMessage));
+                                }
                             }
                         }
                     }
-
                     break;
                 }
             }
@@ -193,41 +310,59 @@ namespace Oxide.Plugins
             player.ChatMessage(message);
         }
 
+        private Dictionary<ulong, DateTime> lastSpawnTimes = new Dictionary<ulong, DateTime>();
+
         private bool HasCooldown(ulong playerID)
         {
-            if (cooldowns.ContainsKey(playerID))
+            if (lastSpawnTimes.ContainsKey(playerID))
             {
-                var cooldownData = cooldowns[playerID];
-                var elapsedTime = DateTime.Now - cooldownData.LastSpawnTime;
-                return elapsedTime.TotalMinutes < cooldownDurationMinutes;
+                var elapsedTime = DateTime.Now - lastSpawnTimes[playerID];
+                if (elapsedTime.TotalMinutes >= cooldownDurationMinutes)
+                {
+                    lastSpawnTimes.Remove(playerID);
+                    //Puts("Removed Cooldown for: " + playerID);
+                    SaveCooldownData();
+                    return false;
+                }
+                return true;
             }
             return false;
         }
 
         private void SetCooldown(ulong playerID)
         {
-            if (cooldowns.ContainsKey(playerID))
+            if (lastSpawnTimes.ContainsKey(playerID))
             {
-                cooldowns[playerID].LastSpawnTime = DateTime.Now;
+                lastSpawnTimes[playerID] = DateTime.Now;
             }
             else
             {
-                cooldowns[playerID] = new CooldownData
-                {
-                    PlayerID = playerID,
-                    LastSpawnTime = DateTime.Now
-                };
+                lastSpawnTimes[playerID] = DateTime.Now;
             }
+            SaveCooldownData();
         }
 
-        private string ReplacePlaceholders(string message, BasePlayer player)
+        private string ReplacePlaceholders(BasePlayer player, string message)
         {
             if (player == null || string.IsNullOrEmpty(message)) return message;
 
             string playerName = player.displayName;
             string serverName = ConVar.Server.hostname;
 
-            return message.Replace("{player.name}", playerName).Replace("{server.name}", serverName);
+            return message.Replace("{0}", playerName).Replace("{1}", serverName);
+        }
+
+        public void GetSetItems(BaseEntity entity, string tableName, int minAmount, int maxAmount, float multiplier)
+        {
+            var storageContainer = entity as StorageContainer;
+            if (storageContainer != null)
+            {
+                if (!string.IsNullOrEmpty(tableName))
+                {
+                    storageContainer.inventory?.Clear();
+                    SimpleLootTable?.Call("GetSetItems", storageContainer, tableName, minAmount, maxAmount, multiplier);
+                }
+            }
         }
 
         private void CleanupContainers()
@@ -240,10 +375,10 @@ namespace Oxide.Plugins
                 }
             }
         }
-
-        void Unload()
+        void OnNewSave()
         {
-            CleanupContainers();
+            lastSpawnTimes.Clear();
+            SaveCooldownData();
         }
     }
 }
