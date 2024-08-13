bfc8ea3aee16c2cc56b1c9995ba9edaf45ffc64e
Mabel8686 <139021822+Mabel8686@users.noreply.github.com>
2024-08-13 12:14:20 +0100

    Update NoLockOnRocket.cs

--- NoLockOnRocket.cs
+++ NoLockOnRocket.cs
@@ -1,3 +1,12 @@
+/*
+Copyright © 2024 Mabel
+
+All rights reserved. This plugin is protected by copyright law.
+
+You may not modify, redistribute, or resell this software without explicit written permission from the copyright owner.
+
+For any support please message me directly via Discord `mabel8686`
+*/
 namespace Oxide.Plugins
 {
     [Info("No Lock-On Rocket", "Mabel", "1.0.0")]
